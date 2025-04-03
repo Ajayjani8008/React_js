c5d41ab746d02899cd5e6a2345bc2829104efdd4
Ajayjani8008 <ajaykumar931396@gmail.com>
2025-04-03 13:23:55 +0530

    Correct error

--- React_New_Projects/react_router_project/src/App.jsx
+++ React_New_Projects/react_router_project/src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
-import Home from './components/Home'
-import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-      <Header />
-      <Home />
-      <Footer />
     </>
   )
 }
